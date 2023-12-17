717bcc4e76da6e3003a1bfc072e41337d3270882
dali <d1999.ali@gmail.com>
2023-12-17 18:45:29 +0100

    get_next_line no norminette

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,133 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 6 
+
+size_t	ft_strlen(char *str)
+{
+	size_t	len;
+	
+	len = 0;
+	while (*str++)
+		len++;
+	return len;
+}
+
+void	ft_strcpy(char *dst, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+}
+
+void	ft_strcat(char *dst, char *src)
+{
+	while (*dst)
+		dst++;
+	ft_strcpy(dst, src);
+}	
+	
+char	*ft_strchrnl(char *str)
+{
+	while (*str)
+	{
+		if (*str == '\n')
+			return str;
+		str++;
+	}
+	return NULL;
+}
+
+char	*ft_substr(char *str, size_t len)
+{
+	size_t	i;
+	char	*sub;
+	
+	sub = (char *)malloc(len + 1);
+	i = 0;
+	while (i < len)
+	{
+		sub[i] = str[i];
+		i++;
+	}
+	sub[i] = '\0';
+	return sub;
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*result;
+
+	result = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	ft_strcpy(result, s1);
+	ft_strcat(result, s2);
+	return result;
+}
+
+char	*ft_strdup(char *str)
+{
+	char	*dup;
+	
+	dup = (char *)malloc(ft_strlen(str) + 1);
+	ft_strcpy(dup, str);
+	return dup;
+}
+
+char	*get_next_line(int fd)
+{
+	char		buff[BUFFER_SIZE + 1];
+	static char	prev_line[BUFFER_SIZE + 1];
+	char		*next_line;
+	char		*tmp_line;
+	char		*sub_line;
+	char		*nl;
+	int		br;
+	
+	if (( nl = ft_strchrnl(prev_line)))
+	{
+		next_line = ft_substr(prev_line, nl - prev_line);	
+		ft_strcpy(prev_line, nl + 1);
+		return next_line;
+	}
+	next_line = ft_strdup(prev_line);
+	while (( br = read(fd, buff, BUFFER_SIZE)))
+	{
+		buff[br] = '\0';
+		if ((nl = ft_strchrnl(buff)))
+		{
+			sub_line = ft_substr(buff, nl - buff);
+			tmp_line = next_line;
+			next_line = ft_strjoin(next_line, sub_line);
+			free(sub_line);
+			free(tmp_line);
+			ft_strcpy(prev_line, nl + 1);
+			return next_line;	
+		}
+		tmp_line = next_line;
+		next_line = ft_strjoin(next_line, buff);
+		free(tmp_line);
+	}
+	return next_line;	
+}
+
+int main()
+{
+	int fd = open("test.txt", O_RDONLY);
+	char *line;
+	for (int i = 0; i < 3; i++)
+	{
+		line = get_next_line(fd);
+		printf("NEXT LINE => %s\n", line);
+		free(line);
+	} 
+	close(fd);
+	return 0;
+}
