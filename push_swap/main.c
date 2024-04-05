d72b8800b0f3e49744300ac5fa8e39c1dd8f1bee
adouiyeh <adouiyeh@c3r7p10.1337.ma>
2024-04-05 08:16:52 +0000

    push_swap actions

--- push_swap/main.c
+++ push_swap/main.c
@@ -1,4 +1,4 @@
-#include "stack.h"
+#include "actions.h"
 
 int main()
 {
@@ -7,9 +7,11 @@ int main()
     t_stack *stack_a = stack_init('a');
     stack_copy(stack_a, vals, 10);
     stack_dump(stack_a);
-    stack_rotate(stack_a);
-    stack_dump(stack_a);
-    stack_rrotate(stack_a);
+
+    ra(stack_a);
+    ra(stack_a);
+    rra(stack_a);
+    rra(stack_a);
     stack_dump(stack_a);
     stack_free(stack_a);
 }
--- push_swap/stack.h
+++ push_swap/stack.h
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 typedef struct s_node
 {
